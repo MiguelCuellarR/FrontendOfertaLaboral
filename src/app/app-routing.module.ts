51cd7a1d5cd6be4748af74c3342b407613047d76
Miguel Angel Cuellar Romero <miguel.1701823659@ucaldas.edu.co>
2021-04-18 17:53:11 -0500

    Enrutamiento y Formulario de Identificacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParametrizacionModule } from './modulos/parametrizacion/parametrizacion.module';
+import { SeguridadModule } from './modulos/seguridad/seguridad.module';
+import { UsuarioModule } from './modulos/usuario/usuario.module';
+import { Error404Component } from './publico/errores/error404/error404.component';
+import { InicioComponent } from './publico/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/inicio' 
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(() => SeguridadModule)
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('./modulos/usuario/usuario.module').then(() => UsuarioModule)
+  },
+  {
+    path: 'parametrizacion',
+    loadChildren: () => import('./modulos/parametrizacion/parametrizacion.module').then(() => ParametrizacionModule)
+  },
+  {
+    path: '**',
+    component: Error404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
